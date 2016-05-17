5dd8b55b5dee85d39a9e3dcb4987c901135275ac
Hroi Sigurdsson <hroi@asdf.dk>
2016-05-17 22:47:52 +0200

    libc -> std::os::raw. Disable test.

--- src/tests.rs
+++ src/tests.rs
@@ -1,9 +1,8 @@
 use libc;
-use std::mem;
-use std::ptr;
+// use std::mem;
+// use std::ptr;
 use std::ffi::CString;
 use super::*;
-//use super::auto;
 
 
 #[test]
@@ -22,52 +21,52 @@ fn test_read_objid() {
     }
 }
 
-#[test]
-fn test_snmp_session() {
-    init(b"test");
-    let peer_name = CString::new("edgy.asdf.dk").unwrap().into_raw();
-    let peer_community = b"st0vsuger";
-    let peer_obj = CString::new("SNMPv2-MIB::sysDescr.0").unwrap().into_raw();
+// #[test]
+// fn test_snmp_session() {
+//     init(b"test");
+//     let peer_name = CString::new("edgy.asdf.dk").unwrap().into_raw();
+//     let peer_community = b"st0vsuger";
+//     let peer_obj = CString::new("SNMPv2-MIB::sysDescr.0").unwrap().into_raw();
 
-    unsafe {
-        let mut sess: Struct_snmp_session = mem::uninitialized();
-        let pdu: *mut Struct_snmp_pdu;
-        let mut response_ptr: *mut Struct_snmp_pdu = mem::uninitialized();
+//     unsafe {
+//         let mut sess: Struct_snmp_session = mem::uninitialized();
+//         let pdu: *mut Struct_snmp_pdu;
+//         let mut response_ptr: *mut Struct_snmp_pdu = mem::uninitialized();
 
-        snmp_sess_init(&mut sess);
-        sess.peername = peer_name;
-        sess.community = &mut peer_community.clone()[0];
-        sess.community_len = peer_community.len();
+//         snmp_sess_init(&mut sess);
+//         sess.peername = peer_name;
+//         sess.community = &mut peer_community.clone()[0];
+//         sess.community_len = peer_community.len();
 
-        sess.version = SNMP_VERSION_2c;
+//         sess.version = SNMP_VERSION_2c;
 
-        let ss = snmp_open(&mut sess);
+//         let ss = snmp_open(&mut sess);
 
-        pdu = snmp_pdu_create(SNMP_MSG_GET);
+//         pdu = snmp_pdu_create(SNMP_MSG_GET);
 
-        let mut anOID: [oid; MAX_OID_LEN] = [0; MAX_OID_LEN];
-        let mut anOID_len = MAX_OID_LEN;
-        if snmp_parse_oid(peer_obj, &mut anOID[0], &mut anOID_len) == ptr::null_mut() {
-            panic!();
-        }
+//         let mut anOID: [oid; MAX_OID_LEN] = [0; MAX_OID_LEN];
+//         let mut anOID_len = MAX_OID_LEN;
+//         if snmp_parse_oid(peer_obj, &mut anOID[0], &mut anOID_len) == ptr::null_mut() {
+//             panic!();
+//         }
 
-        snmp_add_null_var(pdu, &mut anOID[0], anOID_len);
+//         snmp_add_null_var(pdu, &mut anOID[0], anOID_len);
 
-        let status = snmp_synch_response(ss, pdu, &mut response_ptr);
-        println!("snmp_synch_response() -> {}", status);
+//         let status = snmp_synch_response(ss, pdu, &mut response_ptr);
+//         println!("snmp_synch_response() -> {}", status);
 
-        if status == STAT_SUCCESS {
-            let response = *response_ptr;
-            if response.errstat == SNMP_ERR_NOERROR {
-                let mut vars = response.variables;
-                while vars != ptr::null_mut() {
-                    print_variable((*vars).name, (*vars).name_length, vars);
-                    vars = (*vars).next_variable;
-                }
-            }
-        } else {
-            panic!("status={}", status);
-        }
-    }
+//         if status == STAT_SUCCESS {
+//             let response = *response_ptr;
+//             if response.errstat == SNMP_ERR_NOERROR {
+//                 let mut vars = response.variables;
+//                 while vars != ptr::null_mut() {
+//                     print_variable((*vars).name, (*vars).name_length, vars);
+//                     vars = (*vars).next_variable;
+//                 }
+//             }
+//         } else {
+//             panic!("status={}", status);
+//         }
+//     }
 
-}
+// }
